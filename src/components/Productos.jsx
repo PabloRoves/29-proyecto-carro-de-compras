027d03782ccdd5dd7084727c6f2805a1c6cd3cd1
Pablo Rovés <pablo.roves@gmail.com>
2024-05-31 18:02:45 -0300

    Se implementa subsección 190 - Dando estilo a los productos. Se agrega estilo a los componentes productos y producto mediante el uso de inline style.

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,11 +1,19 @@
 import { Component } from "react"
 import Producto from './Producto';
 
+const style = {
+  productos: {
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+}
+
 class Productos extends Component {
   render() {
     const { productos, agregarAlCarro } = this.props;
     return (
-      <div>
+      <div style={style.productos}>
         {productos.map(producto => 
           <Producto
             agregarAlCarro={agregarAlCarro}
