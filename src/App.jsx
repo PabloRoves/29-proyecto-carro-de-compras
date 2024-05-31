61a1f9a5277b5f3353288a9b848903d66be5dacf
Pablo Rovés <pablo.roves@gmail.com>
2024-05-31 17:06:40 -0300

    hasta subseccion 189 - Listando los productos. Se crean los componentes Productos y Producto. Se modifica componente APP, se le agrega estado con los datos de los 3 productos -tomates- -arbejas- -lechuga-, cada uno de ellos con nombre, precio, url a la imágen y se listan en pantalla.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Component } from 'react'
-import './App.css'
+import Productos from './components/Productos.jsx'
 
 class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img:'/productos/tomates.jpeg' },
+      { name: "Arbejas", price: 2000, img:'/productos/arbejas.jpeg' },
+      { name: "Lechuga", price: 500, img:'/productos/lechuga.jpeg' },
+    ]
+  }
+
   render(){
     return (    
       <div>
-        <p>Hola mundo</p>
+        <Productos 
+          agregarAlCarro={() => console.log('No hace nada')}
+          productos={this.state.productos}
+        />
       </div>
     )
   }
