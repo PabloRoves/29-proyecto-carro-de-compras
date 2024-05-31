5173b8b8c7413e211ff54e3e06f172eb9aae2a3a
Pablo Rovés <pablo.roves@gmail.com>
2024-05-31 18:42:28 -0300

    Se implementa subsección 191 - Botón para agregar al carro.

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react"
+import Button from './Button'
 
 const styles = {
   producto: {
@@ -15,12 +16,18 @@ const styles = {
 
 class Producto extends Component {
   render() {
-    const {producto }= this.props;
+    const {producto, agregarAlCarro}= this.props;
+    console.log(agregarAlCarro);
+    console.log(producto);
+
     return (
       <div style={styles.producto}>
         <img style={styles.img} alt={producto.name} src={producto.img}  />
         <h3>{ producto.name}</h3>
         <p>{ producto.price}</p>
+        <Button onClick={() => agregarAlCarro(producto)}>
+          Agregar al carro
+        </Button>
       </div>
     )
   }
