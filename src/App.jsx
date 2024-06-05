88bb136460ec5a38e91b0b8cd2bc1f235849f8fa
Pablo Rovés <pablo.roves@gmail.com>
2024-06-05 18:06:37 -0300

    Se implementa subsección 199 - Agregando elementos al carrito parte 2. Se prueba implementando agregarAlCarro de diferentes formas.

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,99 @@ class App extends Component {
     ],
   }
 
+  /*
+  //Esta implementación de agregarAlCarro no estaría siguiendo el principio de inmutabilidad ya que
+  //estamos modificando directamente el valor de cantidad del producto en el array "carro" en el state
+  //de APP, lo cual provocaría por ejemplo, que React no se entere que tiene que volver a renderizar.
   agregarAlCarro = (producto) => {
-    //console.log(this)
-    //console.log(producto)
+    const { carro } = this.state;
+    const productoEncontrado = carro.find(x => x.name === producto.name);
+    //console.log(productoEncontrado);
+    if (productoEncontrado){
+      productoEncontrado.cantidad = productoEncontrado.cantidad + 1;
+      return;
+    }
     return this.setState({
       carro:this.state.carro.concat({
         ...producto,
         cantidad: 1,
       })
-    })
+    });
   }
+*/
+
+/*
+//En esta implementación, buscamos si ya existe el producto en el array carro.
+//Si existe, copiamos todos los productos del carro a un nuevo carro, menos el del producto
+//que estamos agregando, en su caso, creamos un nuevo objeto producto con la cantidad + 1
+//del producto anterior y lo agregamos al nuevo carro. Por ultimo, asignamos este nuevo carro
+//al estado de app con SetState.
+//(si es que ya existe en el array), 
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find(x => x.name === producto.name)){
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad +1
+        })
+        : x
+      );
+      return this.setState({ carro: newCarro });
+    }
+    return this.setState({
+      carro:this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    });
+  }*/
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    let encontre = false;
+    let newCarro = []
+    newCarro = carro.map(x => {
+      if(x.name === producto.name){
+        encontre = true;
+        const nuevoProducto = {...x, cantidad: x.cantidad + 1}
+        //console.log(nuevoProducto)
+        return nuevoProducto
+      }
+      else{
+        console.log("Agrego al array producto existente")
+        return x;
+      }
+    })
+    //console.log("asd" + {newCarro})
+    //console.log(newCarro)
+    if (!encontre) {
+      const nuevoProducto = {...producto, cantidad: 1,}
+      newCarro = carro.concat(nuevoProducto)
+    }
+    this.setState({carro: newCarro});    
+  }
+
+/*
+    if (carro.find(x => x.name === producto.name)){
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad +1
+        })
+        : x
+      );
+      return this.setState({ carro: newCarro });
+    }
+    return this.setState({
+      carro:this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    });
+
+  }
+  */
 
 
   render(){
